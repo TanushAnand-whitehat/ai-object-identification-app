f861e495d0592963eb1ab75841f5d5eb97c8e415
TanushAnand-whitehat <83594845+TanushAnand-whitehat@users.noreply.github.com>
2022-09-01 18:33:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,13 +7,23 @@ function preload() {
 function setup() {
     canvas = createCanvas(600, 400);
     canvas.center;
+    object_detector = ml5.objectDetector('cocossd', modelLoaded);
+}
+function modelLoaded() {
+    console.log('Model has loaded')
 }
 
 function draw() {
     image(image1, 0, 0, 600, 400);
     fill("red");
     stroke("red");
-    text("dog", 60, 70);
+    text("Dog", 80, 70);
     noFill();
     rect(20, 30, 350, 250);
+
+    fill("red");
+    stroke("red");
+    text("Cat", 220, 70);
+    noFill();
+    rect(190, 20, 350, 250);
 }
